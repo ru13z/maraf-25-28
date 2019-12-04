819d8697e576f4c417a6d916135be83aa89e06d4
Ruslan Zoriichuk <rusikz94@gmail.com>
2019-12-04 23:18:06 +0200

    працюємо над калькулятором

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const form = $('#price-form');
+
+let formData = form.serializeJSON();
+console.log(formData.type);
+
+form.on('keyup change', '', '')
+
+
+
+
+// form.click(function () {
+//     console.log('Click!');
+// });
+
+
+// if () {
+
+// }
